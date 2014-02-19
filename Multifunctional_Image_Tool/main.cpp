51d8f16b72152df202a51c16a5283f8871dcc804
Niels Mayer <Niels.Mayer AT Gmail DOT com>
2014-02-18 18:49:39 -0800

    Changed static allocations to dynamic, to workaround crashes caused by attempting to deallocate static objects when program is closed by user.

--- Multifunctional_Image_Tool/main.cpp
+++ Multifunctional_Image_Tool/main.cpp
@@ -24,30 +24,41 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 #endif /* QT_VERSION */
 
-  //  QZXing::registerQMLTypes();
+//  QZXing::registerQMLTypes();
 
-    ImageHandler imageHandler("imageRepository");
+    // NPM: original static allocation had issue calling superclass destructor on app close
+    //  ~ImageHandler() --> ~QQuickImageProvider() -> free ==> crash :
+    //    D/Qt      (30180): qrc:///main.qml:25 (): MultiFunctionImageTool application closing ...
+    //    D/Qt      (30180): qrc:///main.qml:26 (): MultiFunctionImageTool visible=false
+    //    F/libc    (30180): invalid address or address of corrupt block 0x60c15ed0 passed to dlfree
+    //    F/libc    (30180): Fatal signal 11 (SIGSEGV) at 0xdeadbaad (code=1), thread 30195 (nctionImageTool)
+    // Instead of frobbing the ImageHandler:~ImageHandler() to discern between static and
+    // dynamically allocated ImageHandler, seems easier to just dynamically allocate.
+    ImageHandler* imageHandler = new ImageHandler("imageRepository");
 
+    // NPM original static allocation had issue calling superclass destructor on app close
+    // ~QQuickView() -> ~QQuickWindow() -> segfault
+    // so again, use dynamic allocation of QQuickView,
 #if QT_VERSION >= 0x050000
-    QQuickView           viewer;
+    QQuickView*           viewer = new QQuickView();
 #else
-    QmlApplicationViewer viewer;
+    QmlApplicationViewer* viewer = new QmlApplicationViewer();
 #endif
 
-    viewer.rootContext()->setContextProperty("imageHandler", &imageHandler);
+    viewer->rootContext()->setContextProperty("imageHandler", imageHandler);
     // NPM -- pass viewer to QML -- viewer.width and viewer.height give display dimensions
-    viewer.rootContext()->setContextProperty("viewer", &viewer);
-    viewer.engine()->addImageProvider(imageHandler.providerName(), &imageHandler);
+    viewer->rootContext()->setContextProperty("viewer", viewer);
+    viewer->engine()->addImageProvider(imageHandler->providerName(), imageHandler);
 #if QT_VERSION >= 0x050000
-    viewer.setSource(QUrl("qrc:///main.qml"));
+    viewer->setSource(QUrl("qrc:///main.qml"));
 #if (defined(Q_OS_ANDROID) || defined(Q_OS_LINUX_TIZEN) || defined(Q_OS_LINUX_TIZEN_MOBILE) || defined(Q_OS_LINUX_TIZEN_SIMULATOR) || defined(Q_OS_LINUX_TIZEN_IVI))
-    viewer.showFullScreen();
+    viewer->showFullScreen();
 #else // Desktop, etc.
-    viewer.show();
+    viewer->show();
 #endif /* Qt5 Q_OS_ANDROID ... */
 #else //orig code from Qt4 version
-    viewer.setMainQmlFile(QLatin1String("qml/MultiFunctionImageTool/main.qml"));
-    viewer.showExpanded();
+    viewer->setMainQmlFile(QLatin1String("qml/MultiFunctionImageTool/main.qml"));
+    viewer->showExpanded();
 #endif
 
     return app.exec();
