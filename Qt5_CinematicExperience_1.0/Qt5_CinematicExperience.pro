9d11fa9412d55ac4d6d26de3bea262ca530d014e
Niels Mayer <Niels.Mayer AT Gmail DOT com>
2014-04-24 16:11:15 -0700

    (1) Made platform-ifdef'd changes to main.cpp to be able to launch on Tizen-mobile; (2) added 'shared' and 'tizen' directories only needed for Tizen-mobile deployment under QtCreator; (3) In main.cpp, changed stack-allocated Qt objects to be heap-allocated, preventing core-dump on certain platforms due to what appears to be a new QML bug in versions of Qt newer than 5.1 -- specifically seen on Tizen using 5.2.2/Desktop and Desktop/android using 5.3.0 beta.

--- Qt5_CinematicExperience_1.0/Qt5_CinematicExperience.pro
+++ Qt5_CinematicExperience_1.0/Qt5_CinematicExperience.pro
@@ -1,10 +1,23 @@
 TEMPLATE = app
-
+TARGET = qt5cinematic
 QT += qml quick
 SOURCES += main.cpp
+OTHER_FILES = content/*.qml
 RESOURCES += Qt5_CinematicExperience-default.qrc
 
-target.path = /opt/Qt5_CinematicExperience
-qml.files = Qt5_CinematicExperience.qml content
-qml.path = /opt/Qt5_CinematicExperience
-INSTALLS += target qml
+#target.path = /opt/Qt5_CinematicExperience
+#qml.files = Qt5_CinematicExperience.qml content
+#qml.path = /opt/Qt5_CinematicExperience
+#INSTALLS += target qml
+
+linux-g++|linux-g++-64 {
+   message("Finalise Makefile for 'linux-g++|linux-g++-64'")
+}
+tizen {
+    message("Finalise Makefile for Tizen Mobile on ARM. Reminder: don't use for x86 emulator!")
+    OTHER_FILES += tizen/manifest.xml
+    tizen_shared.files = $$_PRO_FILE_PWD_/shared/res
+    CONFIG += TIZEN_STANDALONE_PACKAGE
+    TIZEN_BUNDLED_QT_LIBS=Qt5Core Qt5DBus Qt5Qml Qt5Quick Qt5QuickParticles Qt5Widgets Qt5Gui Qt5Network Qt5Sensors
+    load(tizen_app)
+}
--- Qt5_CinematicExperience_1.0/main.cpp
+++ Qt5_CinematicExperience_1.0/main.cpp
@@ -1,23 +1,49 @@
 #include <QGuiApplication>
 #include <QQuickView>
 
+#ifdef Q_OS_LINUX_TIZEN_MOBILE
+extern "C" int OspMain(int argc, char *argv[]);
+#endif /* Q_OS_LINUX_TIZEN_MOBILE */
+
 int main(int argc, char* argv[])
 {
-    QGuiApplication app(argc,argv);
-    QQuickView view;
-    view.setResizeMode(QQuickView::SizeRootObjectToView);
+#ifdef Q_OS_LINUX_TIZEN_MOBILE
+    return OspMain(argc,argv);
+}
+
+/* NB: Tizen Mobile, but not Desktop (haven't checked emulator) requires
+ * this indirection via 'OspMain() otherwise the application will not launch
+ * from the desktop icon.
+ * (However, it can be launched directly from /opt/apps/.../bin/qt5cinematic.exe as root from shell)
+ */
+extern "C" int OspMain(int argc, char *argv[])
+{
+#endif /* Q_OS_LINUX_TIZEN_MOBILE */
+
+#ifdef Q_OS_LINUX_TIZEN_SIMULATOR
+    qputenv("QSG_RENDER_LOOP","windows");
+#endif /* Q_OS_LINUX_TIZEN_SIMULATOR */
+#ifndef Q_OS_ANDROID
+    qputenv("QT_QUICK_CONTROLS_STYLE","Tizen");
+#endif
+
+    //NB: if these are stack allocated, instead of heap allocated, you get a core-dump on exit
+    //on some platforms. IMHO this is a new bug introduced after Qt5.1 for QML-based apps.
+    QGuiApplication* app  = new QGuiApplication(argc,argv); //NB: never free()'d, exists for lifetime of app
+    QQuickView*      view = new QQuickView();               //NB: never free()'d, exists for lifetime of app
+    view->setResizeMode(QQuickView::SizeRootObjectToView);
 
 #if QT_VERSION >= 0x050000
-    view.setSource(QUrl("qrc:///Qt5_CinematicExperience.qml"));
+    view->setSource(QUrl("qrc:///Qt5_CinematicExperience.qml"));
 #else
-    view.setSource(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + QLatin1String("/Qt5_CinematicExperience.qml")));
+    view->setSource(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + QLatin1String("/Qt5_CinematicExperience.qml")));
 #endif /* QT_VERSION ... */
 
     const QString lowerArgument = QString::fromLatin1(argv[1]).toLower();
     if (lowerArgument == QLatin1String("--fullscreen")) {
-        view.showFullScreen();
+        view->showFullScreen();
     } else {
-        view.show();
+        view->show();
     }
-    return app.exec();
+    return app->exec();
 }
